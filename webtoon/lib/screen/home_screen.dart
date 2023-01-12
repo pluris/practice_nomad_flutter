dbe68e67415b76cfb1c198a71bd61e8c448023be
pluris <zighart8456@naver.com>
2023-01-13 01:09:14 +0900

    imple homescreen
    Horizontal scroll되는 ListView로 웹툰 뷰들을 구현

--- webtoon/lib/screen/home_screen.dart
+++ webtoon/lib/screen/home_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../models/webtoon_model.dart';
+import '../service/api_service.dart';
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +19,65 @@ class HomeScreen extends StatelessWidget {
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400)),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: 200,
+                ),
+                Expanded(child: makeList(snapshot)),
+              ],
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              child: Image.network(webtoon.thumb),
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) {
+        return const SizedBox(width: 40);
+      },
     );
   }
 }
